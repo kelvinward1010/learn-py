a92a32a4fc77eb5442e930f0bb032a8c779d7c5a
kelvin ward <canigetyournumber101010@gmail.com>
2023-11-19 10:47:00 +0700

    up

--- dictionaries_py.py
+++ dictionaries_py.py
@@ -235,23 +235,23 @@
 # print(myfamily, type(myfamily))
 
 #OR
-child1 = {
-  "name" : "Emil",
-  "year" : 2004
-}
-child2 = {
-  "name" : "Tobias",
-  "year" : 2007
-}
-child3 = {
-  "name" : "Linus",
-  "year" : 2011
-}
+# child1 = {
+#   "name" : "Emil",
+#   "year" : 2004
+# }
+# child2 = {
+#   "name" : "Tobias",
+#   "year" : 2007
+# }
+# child3 = {
+#   "name" : "Linus",
+#   "year" : 2011
+# }
 
-myfamily2 = {
-  "child1" : child1,
-  "child2" : child2,
-  "child3" : child3
-}
-print(myfamily2, type(myfamily2))
-print(myfamily2["child2"]["name"])
+# myfamily2 = {
+#   "child1" : child1,
+#   "child2" : child2,
+#   "child3" : child3
+# }
+# print(myfamily2, type(myfamily2))
+# print(myfamily2["child2"]["name"])
