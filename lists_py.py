b5cb910b312bd4ab4c27e47327a693a36aaae283
kelvin ward <canigetyournumber101010@gmail.com>
2023-11-19 10:17:06 +0700

    up

--- lists_py.py
+++ lists_py.py
@@ -51,3 +51,179 @@
 #   print("Yes, 'apple' is in the fruits list")
 # else:
 #   print("No, 'apple' is not in the fruits list")
+
+#III. Change List Items
+#1. Change Item Value
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+#2. Change a Range of Item Values
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:2] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:3] = ["watermelon"]
+# print(thislist)
+
+
+#IV. Add List Items
+#1. Extend List
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+
+#2. Add Any Iterable
+# thislist = ["apple", "banana", "cherry"]
+# thistuple = ("kiwi", "orange")
+# thisdict = {"kiwi": 8, "orange": 9}
+# thisset = {"kiwi", "orange"}
+# thislist.extend(thistuple)
+# thislist.extend(thisdict)
+# thislist.extend(thisset)
+# print(thislist)
+
+#V. Remove List Items
+#1.Remove Specified Item
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+
+#2. Remove Specified Index
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop(1)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop() # remove last items
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist[0]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist
+
+#VI. Loop Lists
+#1. Loop Through a List
+# thislist = ["apple", "banana", "cherry"]
+# for x in thislist:
+#   print(x)
+
+#2. Loop Through the Index Numbers: Use the range() and len() functions to create a suitable iterable.
+# thislist = ["apple", "banana", "cherry"]
+# for i in range(len(thislist)):
+#   print(thislist[i])
+
+
+#3. Using a While Loop
+# thislist = ["apple", "banana", "cherry"]
+# i = 0
+# while i < len(thislist):
+#   print(thislist[i])
+#   i += 1
+
+#4. Looping Using List Comprehension
+# thislist = ["apple", "banana", "cherry"]
+# [print(x) for x in thislist]
+
+#VII. List Comprehension
+#1. list comprehension
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = []
+
+# for x in fruits:
+#     if "a" in x:
+#         newlist.append(x)
+        
+# print(newlist)
+
+#Or
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+# newlist = [x for x in fruits if "a" in x]
+# newTest = [p for p in fruits if "p" in p]
+
+# print(newTest)
+
+# With no if statement:
+# newlist = [x for x in fruits]
+# print(newlist)
+
+#2. use range to create a list
+# newlist = [x for x in range(10)]
+# print(newlist)
+
+# newlist = [x for x in range(10) if x < 5]
+# print(newlist)
+
+#3. Expressions
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+# newlist = [x.upper() for x in fruits]
+# print(newlist)
+
+# newlist = ['hello' for x in fruits]
+# print(newlist)
+
+# newlist = [x if x != "banana" else "orange" for x in fruits]
+# print(newlist)
+
+
+#VIII. Sort Lists
+#1. Sort List Alphanumerically
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort()
+# print(thislist)
+
+#2. Sort Descending
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort(reverse = True)
+# print(thislist)
+
+#IX. Copy Lists
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+# print(mylist)
+
+#OR
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = list(thislist)
+# print(mylist)
+
+
+#X. Join Lists
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+
+# list3 = list1 + list2
+# print(list3)
+
+#OR
+
+# list1 = ["a", "b" , "c"]
+# list2 = [1, 2, 3]
+
+# for x in list2:
+#   list1.append(x)
+
+# print(list1)
+
+#OR 
+# list1 = ["a", "b" , "c"]
+# list2 = [1, 2, 3]
+
+# list1.extend(list2)
+# print(list1)
+
+
+
+###----------------- LIST AND TUPLE AND SET IS THE SAME --------------------###
